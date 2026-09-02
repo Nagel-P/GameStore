cca343ae48124cea41eed1ce3b5b8d581e4b0f41
Nagel <pedronagelalves@gmail.com>
2026-09-02 01:33:40 -0300

    mirations do db

--- app/schemas/product_schema.py
+++ app/schemas/product_schema.py
@@ -5,7 +5,7 @@ class ProductSchema(Schema):
     id = fields.Integer(dump_only=True)
     name = fields.String(required=True, validate=validate.Length(min=2, max=150))
     description = fields.String(required=False, allow_none=True)
-    price = fields.Decimal(required=True, as_number=True, validate=validate.Range(min=0))
+    price = fields.Decimal(required=True, validate=validate.Range(min=0))
     stock = fields.Integer(required=True, validate=validate.Range(min=0))
     category_id = fields.Integer(required=True, validate=validate.Range(min=1))
 
@@ -13,6 +13,6 @@ class ProductSchema(Schema):
 class ProductPatchSchema(Schema):
     name = fields.String(validate=validate.Length(min=2, max=150))
     description = fields.String(allow_none=True)
-    price = fields.Decimal(as_number=True, validate=validate.Range(min=0))
+    price = fields.Decimal(validate=validate.Range(min=0))
     stock = fields.Integer(validate=validate.Range(min=0))
     category_id = fields.Integer(validate=validate.Range(min=1))
